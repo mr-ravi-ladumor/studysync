251c674e866e2f45a52c16a15c4006d45d4b3334
Ravi Ladumor <ladumorravi1@gmail.com>
2025-07-14 11:56:43 +0530

    uploadResource function added

--- backend/src/controllers/resource.controllers.js
+++ backend/src/controllers/resource.controllers.js
@@ -2,10 +2,44 @@ import asyncHandler from '../utils/asyncHandler.js';
 import ApiError from '../utils/ApiError.js';
 import ApiResponse from '../utils/ApiResponse.js';
 import {allowedMimeTypes} from '../utils/validateFile.js';
+import Resource from '../models/resource.model.js';
 
 
 const uploadResource = asyncHandler( async (req, res) => {
-    
+    const {title, resourceType, subject} = req.body;
+    if(!req.file) {
+        throw new ApiError(400, 'No file uploaded');
+    }
+
+    if (!allowedMimeTypes.includes(req.file.mimetype)) {
+        throw new ApiError(400, `Invalid file type ${req.file.mimetype}`);
+    }
+
+    if (req.file.size > 10 * 1024 * 1024) {
+        throw new ApiError(400, 'File size exceeds the limit of 10 MB');
+    }
+
+    const resource = await Resource.create({
+        title: title,
+        originalFileName: req.file.originalname,
+        resourceType: resourceType,
+        subject: subject,
+        fileUrl: req.file.path,
+        mimeType: req.file.mimetype,
+        size: req.file.size,
+        owner: req.user._id,
+    });
+    // console.log("Resource Created:", resource);
+
+    if (!resource) {
+        throw new ApiError(500, 'Failed to create resource in database');
+    }
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200, resource, 'Resource uploaded successfully')
+    );
 })
 
 
