5c5dadf87982917457e033466c3a327082a6ead0
Ravi Ladumor <ladumorravi1@gmail.com>
2026-03-26 23:26:31 +0530

    fix: copybox in resource details box's link

--- Frontend/src/components/utility/CopyBox.jsx
+++ Frontend/src/components/utility/CopyBox.jsx
@@ -12,17 +12,26 @@ const CopyBox = ({ text }) => {
   };
 
   return (
-    <div className="relative bg-gray-700 text-white rounded-lg p-4 font-mono text-sm">
+    <div className="text-wrap relative bg-gray-700 border-1 border-gray-500 text-white rounded-lg p-4 font-mono text-sm">
       
       <button
         onClick={handleCopy}
-        className="absolute top-2 right-0 bg-gray-700 hover:bg-gray-600 px-2 py-1 rounded text-xs"
+        className="absolute top-2 right-0 border-1 border-gray-500 bg-gray-700 hover:bg-gray-600 px-2 py-1 rounded text-xs"
       >
-        {copied ? <Check size={14} /> : <Copy size={14} />}
+          {copied ? 
+        <div className="flex items-center">
+          <Check size={14} />
+          <span className="ml-1">Copied</span>
+        </div>
+        : 
+        <div className="flex flex-row">
+          <Copy size={14} />
+          <span className="ml-1">Copy</span>
+        </div>}
       </button>
 
       
-      <pre className="whitespace-pre-wrap break-all mr-5">
+      <pre className="whitespace-pre-wrap  break-all">
         {text}
       </pre>
     </div>
