b10ead97241528485caadb7004b748f00675248a
Ravi Ladumor <ladumorravi1@gmail.com>
2026-03-30 16:07:34 +0530

    nodemailer setup/configuration for reset password service

---
+++ backend/src/config/nodemailer.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILTRAP_HOST,
+  port: process.env.MAILTRAP_PORT,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASS
+  }
+});
+
+export default transporter;
